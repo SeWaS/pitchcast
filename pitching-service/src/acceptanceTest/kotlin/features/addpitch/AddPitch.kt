641eaf4d5b174217d51af2acf1952f35fc677f55
SeWa <sebastian_walter@me.com>
2018-04-23 19:35:30 +0200

    Reimplemented pitchingservice's acceptance tests in kotlin

---
+++ pitching-service/src/acceptanceTest/kotlin/features/addpitch/AddPitch.kt
@@ -0,0 +1,48 @@
+package features.addpitch
+
+import io.github.benas.randombeans.api.EnhancedRandom
+import io.pitchcast.pitchingservice.PitchingServiceApp
+import io.pitchcast.pitchingservice.domain.Pitch
+import io.pitchcast.pitchingservice.domain.repository.PitchesRepository
+import io.pitchcast.pitchingservice.web.dto.PitchDto
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = [PitchingServiceApp::class]
+)
+class AddPitch {
+
+    @Autowired lateinit var testRestTemplate: TestRestTemplate
+    @Autowired lateinit var repository: PitchesRepository
+
+    @AfterEach
+    fun tearDown() = repository.deleteAll()
+
+    @Test
+    fun shouldAddValidPitch() {
+        // given
+        val pitch = EnhancedRandom.random(PitchDto::class.java)
+
+        // when
+        val getPitchesResponse = testRestTemplate.postForEntity("/pitches/", pitch, Long::class.java)
+
+        // then
+        assertThat(getPitchesResponse.statusCodeValue).isEqualTo(201)
+
+        val storedPitch = repository.findById(getPitchesResponse.body).get()
+        assertThat(storedPitch).isEqualToIgnoringGivenFields(pitch, "pitchId", "pitchResult", "pitchType")
+        assertThat(storedPitch.pitchResult.name).isEqualTo(pitch.pitchResult.name)
+        assertThat(storedPitch.pitchType.name).isEqualTo(pitch.pitchType.name)
+    }
+
+}
---
+++ pitching-service/src/acceptanceTest/kotlin/features/getpitch/GetPitch.kt
@@ -0,0 +1,73 @@
+package features.getpitch
+
+import io.github.benas.randombeans.api.EnhancedRandom
+import io.pitchcast.pitchingservice.PitchingServiceApp
+import io.pitchcast.pitchingservice.domain.Pitch
+import io.pitchcast.pitchingservice.domain.repository.PitchesRepository
+import io.pitchcast.pitchingservice.web.dto.PitchDto
+import io.pitchcast.pitchingservice.web.dto.PitchesDto
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.test.context.junit.jupiter.SpringExtension
+
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+@ExtendWith(SpringExtension::class)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = [PitchingServiceApp::class]
+)
+class GetPitch {
+
+    @Autowired lateinit var testRestTemplate: TestRestTemplate
+    @Autowired lateinit var repository: PitchesRepository
+
+    @AfterEach
+    fun tearDown() = repository.deleteAll()
+
+    @Test fun shouldReturnAllPitches() {
+        // given
+        val testPitches = EnhancedRandom.randomListOf(3, Pitch::class.java)
+        repository.saveAll(testPitches)
+        repository.flush()
+
+        // when
+        val getPitchesResponse = testRestTemplate.getForEntity("/pitches/", PitchesDto::class.java)
+
+        // then
+        assertThat(getPitchesResponse.statusCodeValue).isEqualTo(200)
+        assertThat(getPitchesResponse.body.pitches).hasSize(3)
+    }
+
+    @Test fun shouldReturnPitchesByPitcher() {
+        // given
+        val PITCHER = "NAME OF PITCHER"
+
+        val testPitches = EnhancedRandom.randomListOf(3, Pitch::class.java)
+        repository.saveAll(testPitches)
+        repository.flush()
+
+        val pitchForPitcher = EnhancedRandom.random(Pitch::class.java)
+        pitchForPitcher.pitcherId = 123L
+        pitchForPitcher.pitcherName = PITCHER
+        repository.saveAndFlush(pitchForPitcher)
+
+        // when
+        val getPitchesResponse = testRestTemplate.getForEntity("/pitches/123", PitchesDto::class.java)
+
+        // then
+        assertThat(getPitchesResponse.statusCodeValue).isEqualTo(200)
+        assertThat<PitchDto>(getPitchesResponse.body.pitches).hasSize(1)
+
+        val receivedPitch = getPitchesResponse.body.pitches[0]
+
+        assertThat(receivedPitch).isEqualToIgnoringGivenFields(pitchForPitcher, "pitchId", "pitchType", "pitchResult")
+        assertThat(receivedPitch.pitchResult.name).isEqualTo(pitchForPitcher.pitchResult.name)
+        assertThat(receivedPitch.pitchType.name).isEqualTo(pitchForPitcher.pitchType.name)
+    }
+
+}
