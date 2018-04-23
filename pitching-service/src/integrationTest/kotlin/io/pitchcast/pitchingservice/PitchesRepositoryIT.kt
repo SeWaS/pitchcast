526570e0289394839c4ba355f993f794a26686b7
SeWa <sebastian_walter@me.com>
2018-04-23 18:48:00 +0200

    Reimplemented pitchingservice's integration tests in kotlin

---
+++ pitching-service/src/integrationTest/kotlin/io/pitchcast/pitchingservice/PitchesRepositoryIT.kt
@@ -0,0 +1,78 @@
+package io.pitchcast.pitchingservice
+
+import io.github.benas.randombeans.api.EnhancedRandom
+import io.pitchcast.pitchingservice.domain.Pitch
+import io.pitchcast.pitchingservice.domain.repository.PitchesRepository
+import io.pitchcast.pitchingservice.service.PitchesService
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.context.annotation.Import
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import javax.validation.ConstraintViolationException
+
+@ExtendWith(SpringExtension::class)
+@ActiveProfiles("mysql")
+@Import(PitchesService::class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class PitchesRepositoryIT {
+
+    @Autowired lateinit var pitchesService: PitchesService
+    @Autowired lateinit var pitchesRepository: PitchesRepository
+
+    @Test fun shouldStoreValidPitchCorrectly() {
+        // given
+        val validPitch = EnhancedRandom.random(Pitch::class.java)
+
+        // when
+        pitchesService.saveNewPitch(validPitch)
+
+        // then
+        val foundPitch = pitchesService.getAllPitches().get(0)
+        assertThat(foundPitch).isEqualToIgnoringGivenFields(validPitch, "pitchId", "pitchResult", "pitchType")
+        assertThat(foundPitch.getPitchResult().name).isEqualTo(validPitch.pitchResult.name)
+        assertThat(foundPitch.getPitchType().name).isEqualTo(validPitch.pitchType.name)
+    }
+
+    @Test fun shouldNotStoreInvalidPitchCorrectly() {
+        // given
+        val invalidPitch = Pitch()
+
+        // when
+        assertThatExceptionOfType(ConstraintViolationException::class.java).isThrownBy(
+                { pitchesService.saveNewPitch(invalidPitch) }
+        )
+    }
+
+    @Test fun shouldFindPitchesByPitcherId() {
+        // given
+        val PITCHER_ID = 9876L
+
+        val pitch1 = EnhancedRandom.random(Pitch::class.java)
+        pitchesRepository.saveAndFlush(pitch1)
+
+        val pitch2 = EnhancedRandom.random(Pitch::class.java)
+        pitchesRepository.saveAndFlush(pitch2)
+
+        val pitch3 = EnhancedRandom.random(Pitch::class.java)
+        pitch3.pitcherId = PITCHER_ID
+        pitchesRepository.saveAndFlush(pitch3)
+
+        val pitch4 = EnhancedRandom.random(Pitch::class.java)
+        pitch4.pitcherId = PITCHER_ID
+        pitchesRepository.saveAndFlush(pitch4)
+
+        // when
+        val foundPitches = this.pitchesService.getPitchesByPitcher(PITCHER_ID)
+
+        // then
+        assertThat(foundPitches.size).isEqualTo(2)
+    }
+
+}
---
+++ pitching-service/src/integrationTest/kotlin/io/pitchcast/pitchingservice/web/pitchescontroller/PitchesControllerPitchesIT.kt
@@ -0,0 +1,60 @@
+package io.pitchcast.pitchingservice.web.pitchescontroller
+
+import io.github.benas.randombeans.api.EnhancedRandom
+import io.github.benas.randombeans.api.EnhancedRandom.random
+import io.pitchcast.pitchingservice.domain.Pitch
+import io.pitchcast.pitchingservice.service.PitchesService
+import io.pitchcast.pitchingservice.web.PitchesController
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.given
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.http.MediaType
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.util.*
+
+@ExtendWith(SpringExtension::class)
+@WebMvcTest(controllers = [PitchesController::class])
+class PitchesControllerPitchesIT {
+
+    @Autowired lateinit var mockMvc: MockMvc
+    @MockBean lateinit var pitchesService: PitchesService
+
+    @Test fun shouldReturn200IfWantingAllPitches() {
+
+        given(this.pitchesService.allPitches).willReturn(Arrays.asList<Pitch>(
+                random(Pitch::class.java),
+                random(Pitch::class.java)
+        ))
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/pitches/")
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(MockMvcResultMatchers.status().`is`(200))
+
+        verify(this.pitchesService, times(1)).getAllPitches()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun shouldReturn200IfWantingPitchesByPitcherId() {
+
+        given(this.pitchesService.getPitchesByPitcher(12345L)).willReturn(EnhancedRandom.randomListOf(3, Pitch::class.java))
+
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/pitches/12345")
+                        .accept(MediaType.APPLICATION_JSON)
+        ).andExpect(MockMvcResultMatchers.status().`is`(200))
+
+        verify(this.pitchesService, times(1)).getPitchesByPitcher(12345L)
+
+    }
+
+}
