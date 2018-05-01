96d92f5a064e226c1528805ae3197e805814a7d3
SeWa <sebastian_walter@me.com>
2018-05-01 14:38:50 +0200

    Removed unused imports (thank you, codacy)

--- pitching-service/src/acceptanceTest/kotlin/features/addpitch/AddPitch.kt
+++ pitching-service/src/acceptanceTest/kotlin/features/addpitch/AddPitch.kt
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.test.context.ActiveProfiles
 import testing.AcceptanceTest
 
 @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
--- pitching-service/src/integrationTest/kotlin/io/pitchcast/pitchingservice/PitchesRepositoryIT.kt
+++ pitching-service/src/integrationTest/kotlin/io/pitchcast/pitchingservice/PitchesRepositoryIT.kt
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.context.annotation.Import
-import org.springframework.test.context.ActiveProfiles
 import testing.DataJpaSliceTest
 import javax.validation.ConstraintViolationException
 
