29724e54075c4f1d043ae3b79f9ae1edff5ad486
SeWa <sebastian_walter@me.com>
2018-05-01 14:27:19 +0200

    PitcherService acceptance tests run against real DB

--- pitcher-service/src/acceptanceTest/kotlin/features/addpitcher/AddPitcher.kt
+++ pitcher-service/src/acceptanceTest/kotlin/features/addpitcher/AddPitcher.kt
@@ -8,9 +8,11 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.test.context.ActiveProfiles
 import testing.AcceptanceTest
 
 @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+@ActiveProfiles("postgres")
 @AcceptanceTest(forApp = [PitcherServiceApp::class])
 class AddPitcher {
 
